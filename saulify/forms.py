af0a30dd77ac29f22cabc7d8814c9dd883638385
mel <mel.pieceofchalk@gmail.com>
2014-11-28 02:39:05 +0300

    fix requerements

--- saulify/forms.py
+++ saulify/forms.py
@@ -1,10 +1,4 @@
 from flask.ext.wtf import Form
-from wtforms import TextField, BooleanField, TextAreaField, HiddenField
-from wtforms.validators import Required
-
-
-class GenApiKey(Form):
-    api_key = TextField('api_key', validators=[Required()])
 
 
 class LoginForm(Form):
