3375ae16a0fa9571abe2d55f4dac337a46376cca
Adriano <adrianowalmeid@gmail.com>
2014-12-02 16:14:46 -0200

    debug on heroku

--- config.py
+++ config.py
@@ -3,16 +3,20 @@ import urlparse
 
 CSRF_ENABLED = True
 # heroku config:set SECRET_KEY=your_secret_key
-SECRET_KEY = os.environ.get('SECRET_KEY')
+#SECRET_KEY = os.environ.get('SECRET_KEY')
 # heroku addons:add heroku-postgresql:dev
 # heroku pg:promote HEROKU_POSTGRESQL_{COLOR}_URL
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+#SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
+SECRET_KEY = '***REMOVED***'
+SQLALCHEMY_DATABASE_URI =  '***REMOVED***'
 
 # heroku addons:add rediscloud
-url = urlparse.urlparse(os.environ.get('REDISCLOUD_URL'))
-REDIS_HOST = url.hostname
-REDIS_PORT = url.port
-REDIS_PASS = url.password
+#url = urlparse.urlparse(os.environ.get('REDISCLOUD_URL'))
+#REDIS_HOST = url.hostname
+#REDIS_PORT = url.port
+#REDIS_PASS = url.password
 
-# the limit to rate - tuple (calls, minute)
-RATE_LIMIT_GET = (1,60)
+REDIS_HOST = '***REMOVED***'
+REDIS_PORT = '17421'
+REDIS_PASS = '***REMOVED***'
