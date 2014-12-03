b62454e81983cb21e00c7b1ac8646301fdf2ec97
Adriano <adrianowalmeid@gmail.com>
2014-12-03 07:17:27 -0200

    fixed pass on config.py

--- config.py
+++ config.py
@@ -3,20 +3,13 @@ import urlparse
 
 CSRF_ENABLED = True
 # heroku config:set SECRET_KEY=your_secret_key
-#SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # heroku addons:add heroku-postgresql:dev
 # heroku pg:promote HEROKU_POSTGRESQL_{COLOR}_URL
-#SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-
-SECRET_KEY = '***REMOVED***'
-SQLALCHEMY_DATABASE_URI =  '***REMOVED***'
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 # heroku addons:add rediscloud
-#url = urlparse.urlparse(os.environ.get('REDISCLOUD_URL'))
-#REDIS_HOST = url.hostname
-#REDIS_PORT = url.port
-#REDIS_PASS = url.password
-
-REDIS_HOST = '***REMOVED***'
-REDIS_PORT = '17421'
-REDIS_PASS = '***REMOVED***'
+url = urlparse.urlparse(os.environ.get('REDISCLOUD_URL'))
+REDIS_HOST = url.hostname
+REDIS_PORT = url.port
+REDIS_PASS = url.password
