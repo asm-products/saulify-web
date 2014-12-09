58c568d12d903a048b3724caf3ec1ea1e3d1d24d
Adriano <adrianowalmeid@gmail.com>
2014-12-09 10:40:40 -0200

    added tests for the rate limit

---
+++ config_debug.py
@@ -0,0 +1,11 @@
+__author__ = 'adriano'
+
+# do not change this debug parameters. Change in __init__.py
+DEBUG = True
+
+SECRET_KEY = 'ASDSDAASD'
+SQLALCHEMY_DATABASE_URI =  'postgres://yothhvlrvgfnmv:40ZPCraMqclHpFtURZwLps-Pv-@ec2-23-23-80-55.compute-1.amazonaws.com:5432/d976il7m33b8kn'
+SHORTER_SALT = 'saltsalt'
+REDIS_HOST = 'pub-redis-17421.us-east-1-4.4.ec2.garantiadata.com'
+REDIS_PORT = 17421
+REDIS_PASS = 'Co0t39rdIiwxhGq6'
---
+++ tests/test_limit.py
@@ -0,0 +1,21 @@
+from saulify import app as test_app
+from saulify.common import ratelimit
+
+@test_app.route('/test')
+@ratelimit(2,60)
+def test_route():
+    '''
+    a very simple route that will 
+    '''
+    return 'ok'
+
+def test_shortener(client):
+    # try once and than another 2 times to see if the return is ok.
+    ret = client.get('/test', follow_redirects=True)
+    assert ret.status_code == 200
+
+    ret = client.get('/test', follow_redirects=True)
+    assert ret.status_code == 200
+
+    ret = client.get('/test', follow_redirects=True)
+    assert ret.status_code == 429
