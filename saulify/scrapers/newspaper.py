f4d66a5820582c995f1d31fe6a2442fc42d71077
Adel Foda <adelfoda@gmail.com>
2014-12-28 12:10:49 +1100

    Split `clean_content` into component functions
    Provides ability to use newspaper to parse articles whose source has
    already been downloaded.

--- saulify/scrapers/newspaper.py
+++ saulify/scrapers/newspaper.py
@@ -9,10 +9,53 @@ import html2text
 
 
 def clean_content(url_to_clean):
+    """ Parse an article at a given url using newspaper.
+
+    Args:
+        url (str): Url where the article is found.
+
+    Returns:
+        Dictionary providing cleaned article and extracted content
+        (see `construct_result`).
+    """
 
     article = Article(url_to_clean)
     article.download()
     article.parse()
+
+    return construct_result(article)
+
+
+def clean_source(url, source):
+    """ Parse a pre-downloaded article using newspaper.
+
+    Args:
+        url (str): The url where the article was sourced (necessary for the
+                newspaper API).
+
+        source (str): Html source of the article page.
+
+    Returns:
+        Dictionary providing cleaned article and extracted content
+        (see `construct_result`).
+    """
+    article = Article(url)
+    article.set_html(source)
+    article.parse()
+
+    return construct_result(article)
+
+
+def construct_result(article):
+    """ Construct article extraction result dictionary in standard format.
+
+    Args:
+        article (Article): A parsed `newspaper` `Article` object.
+
+    Returns:
+        Dictionary providing cleaned article and extracted content;
+        author, title, markdown, plaintext, html.
+    """
 
     html_string = ElementTree.tostring(article.clean_top_node)
     markdown = html2text.HTML2Text().handle(html_string)
