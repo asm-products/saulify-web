3dd60e5879fa80ea4971e428615926290f7eb19b
Frederik Creemers <frederikcreemers@gmail.com>
2015-02-04 23:43:45 +0100

    updated signup and login forms

--- saulify/forms.py
+++ saulify/forms.py
@@ -1,16 +1,16 @@
 from flask.ext.wtf import Form
 from wtforms import TextField, PasswordField, \
-    validators, HiddenField
+    validators, HiddenField, Em
 
+
+xclass RegisterForm(Form):
+    email = TextField('Email address', validators=[validators.DataRequired('Please enter an email address.'),
+                                                   validators.Email('Please enter a valid email address.'),])
+    password = PasswordField('Password', validators=[validators.DataRequired('Please enter a password.'),
+                                                 validators.Length(min=8, message='Your password should be at least 8 characters long.')])
+    confirm = PasswordField('Confirm password', validators=[validators.EqualTo('password', 'Your password and password confirmation don\'t match up.')])
 
 class LoginForm(Form):
-    def __init__(self, arg):
-        super(LoginForm, self).__init__()
-        self.arg = arg
-
-
-class AddUserForm(Form):
-    id = HiddenField()
-    email = TextField('Email Address', [validators.Length(min=6, max=35),
-                                        validators.email()])
-    password = PasswordField('Password', [validators.Required()])
+    email = TextField('Email address', validators=[validators.DataRequired('Please enter an email address.')])
+    password = PasswordField('Password', validators=[validators.DataRequired('Please enter a password.')])
+    
